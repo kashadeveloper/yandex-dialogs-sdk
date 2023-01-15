ca1f32fc4636c5eb8d8739290f4fb90810aba9ff
kashadeveloper <79090088+kashadeveloper@users.noreply.github.com>
2023-01-15 11:42:51 +0300

    fix: ISession return in global context
    ISession was returned, but that was in context interface

--- src/context.ts
+++ src/context.ts
@@ -1,17 +1,16 @@
 import { IApiRequest } from './api/request';
 import { IApiRequestNlu } from './api/nlu';
 import { IApiResponseBody } from './api/response';
-
+import { ISession } from './session/session'
 export interface IContext {
-  readonly data: IApiRequest;
-  readonly message: string;
-  readonly originalUtterance: string;
-  readonly sessionId: string;
-  readonly messageId: number;
-  readonly userId: string;
-  readonly payload?: object;
-  readonly nlu?: IApiRequestNlu;
-  // param response appears in context when all
-  // middlewares have been done and we got some response
-  response?: IApiResponseBody;
+    readonly data: IApiRequest;
+    readonly message: string;
+    readonly originalUtterance: string;
+    readonly sessionId: string;
+    readonly session: ISession;
+    readonly messageId: number;
+    readonly userId: string;
+    readonly payload?: object;
+    readonly nlu?: IApiRequestNlu;
+    response?: IApiResponseBody;
 }
